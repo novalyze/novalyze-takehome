cc775f0400d70e0aabbc956aa1351905bb16c85d
Akhilesh Das <akhilesh-das@hotmail.com>
2025-02-25 17:16:12 +0530

    feat: implement basic chatbot area requirements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { type KeyboardEvent, useRef, useState } from "react";
 import Header from '../components/Header';
+
+const Chatbot = () => {
+	const [messages, setMessages] = useState<string[]>([
+		'Test A',
+		'Test B'
+	])
+
+	const inputElRef = useRef<HTMLInputElement>(null)
+
+	const registerMessage = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key !== 'Enter')
+			return
+
+		const buffer = [ ...messages, e.currentTarget.value ]
+		setMessages(buffer)
+
+		inputElRef.current!.value = ''
+	}
+
+	return <>
+		{
+			messages.map((msg, i) => (
+				<div key={i}>{ msg }</div>
+			))
+		}
+		<input ref={inputElRef} type="text" placeholder="Ask me anything"
+			className="border border-indigo-600 p-3"
+			onKeyDown={registerMessage}/>
+	</>
+}
 
 const Home: React.FC = () => {
   return (
     <>
         <Header />
-        <div className="flex h-screen items-center justify-center bg-gray-background-dark">
-        <div className="max-w-lg rounded-xl bg-white p-8 shadow-md">
-            <h1 className="text-3xl font-bold text-gray-900">Welcome to the Home Page</h1>
-            <p className="mt-4 text-gray-600">
-            This is the main landing page of the app.
-            </p>
-        </div>
+        <div className="flex">
+	       <div className="w-80">
+			<Chatbot />
+		   </div>
+		   <div className="bg-gray-background-dark w-full">
+			PDF
+		   </div>
         </div>
     </>
   );
