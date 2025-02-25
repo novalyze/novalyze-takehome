2ded1185de102ef53d39f71d5bdcad0563f9aab8
Akhilesh Das <akhilesh-das@hotmail.com>
2025-02-26 03:03:11 +0530

    feat: implement login and register page redesign

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,64 +2,39 @@ import styles from "./Login.module.scss"
 
 export default function () {
 	return (
-		<>
-			<div
-				className={`${styles.reveal} flex h-screen items-center justify-center bg-gray-background-dark`}
-			>
-				<div className="max-w-md rounded-xl bg-white p-6 shadow-md">
-					<h2 className="text-2xl font-bold text-gray-900">Login</h2>
-					<a href="/register">
-						<button
-							type="button"
-							className="w-1/2 mt-4 rounded-md bg-primary p-2 text-white"
-						>
-							New user
-						</button>
-					</a>
-					<form className="mt-4">
-						<div className="mb-4">
-							<label htmlFor="email" className="block text-gray-700">
-								Email
-							</label>
-							<input
-								name="email"
-								type="email"
-								className="w-full rounded-md border border-gray-300 p-2"
-								placeholder="Enter your email"
-							/>
-						</div>
-						<div className="mb-4">
-							<label htmlFor="password" className="block text-gray-700">
-								Password
-							</label>
-							<input
-								name="password"
-								type="password"
-								className="w-full rounded-md border border-gray-300 p-2"
-								placeholder="Enter your password"
-							/>
-						</div>
-						<button
-							type="submit"
-							className="w-1/2 rounded-md bg-primary p-2 text-white"
-						>
-							Login
-						</button>
-						<button
-							type="button"
-							className="w-1/2 rounded-md bg-primary p-2 text-white"
-						>
+		<div className={`${styles.reveal} ${styles.login}`}>
+			<div className="max-w-md rounded-xl bg-white p-6 shadow-md">
+				<h2 className="text-2xl font-bold text-gray-900">Login</h2>
+				<a className="alternate" href="/register">
+					New user?
+				</a>
+
+				<form className="mt-4 pt-2">
+					<input
+						name="email"
+						type="email"
+						className="w-full rounded-md border border-gray-300 p-2 mb-2"
+						placeholder="Enter your email"
+					/>
+					<input
+						name="password"
+						type="password"
+						className="w-1/2 rounded-md border border-gray-300 p-2 mb-4"
+						placeholder="Enter your password"
+					/>
+
+					<div>
+						<button type="button">Login</button>
+						<button type="button">Sign in with SSO</button>
+					</div>
+
+					<div className="mt-3">
+						<a className="alternate" href="#none">
 							Reset Password
-						</button>
-						<button
-							type="button"
-							className="w-full rounded-md bg-primary mt-2 p-2 text-white"
-						>
-							Sign in with SSO
-						</button>
-					</form>
-				</div>
+						</a>
+					</div>
+				</form>
 			</div>
-		</>
+		</div>
 	)
 }
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -2,52 +2,36 @@ import styles from "./Register.module.scss"
 
 export default function () {
 	return (
-		<>
-			<div
-				className={`${styles.reveal} flex h-screen items-center justify-center bg-gray-background-dark`}
-			>
-				<div className="max-w-md rounded-xl bg-white p-6 shadow-md">
-					<h2 className="text-2xl font-bold text-gray-900">New user</h2>
-					<a href="/login">
-						<button
-							type="button"
-							className="w-1/2 mt-4 rounded-md bg-primary p-2 text-white"
-						>
-							Login
-						</button>
-					</a>
-					<form className="mt-4">
-						<div className="mb-4">
-							<label htmlFor="email" className="block text-gray-700">
-								Email
-							</label>
-							<input
-								name="email"
-								type="email"
-								className="w-full rounded-md border border-gray-300 p-2"
-								placeholder="Enter your email"
-							/>
-						</div>
-						<div className="mb-4">
-							<label htmlFor="password" className="block text-gray-700">
-								Password
-							</label>
-							<input
-								name="password"
-								type="password"
-								className="w-full rounded-md border border-gray-300 p-2"
-								placeholder="Enter your password"
-							/>
-						</div>
-						<button
-							type="button"
-							className="w-full rounded-md bg-primary p-2 text-white"
-						>
-							Create user
-						</button>
-					</form>
-				</div>
+		<div className={`${styles.reveal} ${styles.register}`}>
+			<div className="max-w-md rounded-xl bg-white p-6 shadow-md">
+				<h2 className="text-2xl font-bold text-gray-900">Sign Up</h2>
+
+				<form className="mt-4 pt-2">
+					<input
+						name="email"
+						type="email"
+						className="w-full rounded-md border border-gray-300 p-2 mb-2"
+						placeholder="Enter your email"
+					/>
+					<input
+						name="password"
+						type="password"
+						className="w-1/2 rounded-md border border-gray-300 p-2 mb-4"
+						placeholder="Enter your password"
+					/>
+
+					<div>
+						<button type="button">Login</button>
+						<button type="button">Sign in with SSO</button>
+					</div>
+
+					<div className="mt-3">
+						<a className="alternate" href="/login">
+							Log in with existing credentials
+						</a>
+					</div>
+				</form>
 			</div>
-		</>
+		</div>
 	)
 }
